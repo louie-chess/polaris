112ebde1c553350560aa9166e383465528d59fdf
louie-chess <235987949+louie-chess@users.noreply.github.com>
2026-02-23 17:09:07 +0800

    progress bar and database loading

--- hashtable.py
+++ hashtable.py
@@ -1,18 +1,21 @@
 import zobrist
 import chess
 import numpy as np
+import sqlite3
+from tqdm import tqdm
 
 DEFAULT_SIZE = 10007
 
-class HashTable:
-    def __init__(self, size=DEFAULT_SIZE):
+class EvaluationsHashTable:
+    def __init__(self, size=DEFAULT_SIZE, cur: sqlite3.Cursor=None):
+        self.hash = np.uint64(0)
         self.table = [None] * size
-        self.size = size
-        self.length = 0
-    
-    def hash(self, board: chess.Board):
-        hash = np.array(0, dtype=np.uint64)
+        self.size = np.uint32(size)
+        self.length = np.uint32()
+        if cur is not None:
+            self.load_db(cur)
 
+    def hash_pieces(self, board: chess.Board):
         for square in chess.SQUARES:
             piece = board.piece_at(square)
             if piece is None: continue
@@ -21,26 +24,53 @@ class HashTable:
             piece_type = piece.piece_type - 1
             index = piece_type * 2 * 64 + color * 64 + square
 
-            hash ^= zobrist.hashes[zobrist.PIECE_HASHES + index]
+            self.hash ^= zobrist.hashes[zobrist.PIECE_HASHES + index]
 
-        hash ^= zobrist.hashes[zobrist.TURN_HASHES + (1 if board.turn == chess.WHITE else 0)]
+    def hash_turn(self, board: chess.Board):
+        turn = 1 if board.turn == chess.WHITE else 0
+        self.hash ^= np.uint64(zobrist.hashes[zobrist.TURN_HASHES + turn])
 
+    def hash_castling_rights(self, board: chess.Board):
         for castling_right in range(len(zobrist.CASTLING_RIGHTS)):
             if board.castling_rights & castling_right > 0:
-                hash ^= zobrist.hashes[zobrist.CASTLING_HASHES + castling_right]
+                self.hash ^= zobrist.hashes[zobrist.CASTLING_HASHES + castling_right]
 
-        if board.ep_square is not None:
-            hash ^= zobrist.hashes[zobrist.EP_HASHES + board.ep_square]
+    def hash_ep_square(self, board: chess.Board):
+        if board.ep_square is None:
+            return np.uint64(0)
 
-        return hash
-    
+        self.hash ^= np.uint64(zobrist.hashes[zobrist.EP_HASHES + board.ep_square])
+
+    def reset_hash(self):
+        self.hash = np.uint64(0)
+
+    def hash_board(self, board: chess.Board):
+        self.reset_hash()
+        self.hash_pieces(board)
+        self.hash_turn(board)
+        self.hash_castling_rights(board)
+        self.hash_ep_square(board)
+
+    def load_db(self, cur: sqlite3.Cursor):
+        cur.execute("SELECT zobrist, binary, score FROM evaluations")
+        evaluations = cur.fetchall()
+        self.length = len(evaluations)
+
+        for zobrist, binary, score in tqdm(evaluations, "Loading database"):
+            zobrist = np.frombuffer(zobrist, dtype=np.uint64)[0]
+            index = zobrist % self.size
+
+            self.table[index] = (binary, score)
+
     def __setitem__(self, board, node):
-        index = self.hash(board) % self.size
+        self.hash_board(board)
+        index = self.hash % self.size
         self.table[index] = node
         self.length += 1
 
     def __getitem__(self, board):
-        index = self.hash(board) % self.size
+        self.hash_board(board)
+        index = self.hash % self.size
 
         return self.table[index]
     
--- main.py
+++ main.py
@@ -5,49 +5,65 @@ import utils
 import sqlite3
 from dotenv import load_dotenv
 import os
+import typing
+from tqdm import tqdm
 
 load_dotenv()
 
-def insert_evaluation(board: chess.Board, engine: chess.engine.SimpleEngine, conn: sqlite3.Connection) -> int:
+def insert_evaluation(board: chess.Board, boards: hashtable.EvaluationsHashTable, engine: chess.engine.SimpleEngine, conn: sqlite3.Connection) -> int:
     binary = utils.binary(board)
     info = engine.analyse(board, chess.engine.Limit(depth=10))
     score = info["score"].relative.score(mate_score=100000)
+    boards[board] = (binary, score)
+    zobrist = boards.hash.tobytes()
 
-    conn.execute('INSERT INTO evaluations (binary, score) VALUES (?, ?)', (binary, score))
+    conn.execute(
+        "INSERT INTO evaluations (zobrist, binary, score) VALUES (?, ?, ?)",
+        (zobrist, binary, score))
     conn.commit()
 
     return binary, score
 
-def insert_pgn(pgn, boards: hashtable.HashTable, engine: chess.engine.SimpleEngine, conn: sqlite3.Connection):
+def insert_evaluations(game: chess.pgn.Game, boards: hashtable.EvaluationsHashTable, engine: chess.engine.SimpleEngine, conn: sqlite3.Connection, fn: typing.Callable[[bytes, int], typing.Any]=None):
+    board = game.board()
+    for move in game.mainline_moves():
+        board.push(move)
+        if boards[board] is not None: continue
+
+        binary, score = insert_evaluation(board, boards, engine, conn)
+        if fn is not None: fn(binary, score)
+
+def insert_pgn(pgn, boards: hashtable.EvaluationsHashTable, engine: chess.engine.SimpleEngine, conn: sqlite3.Connection, pbar: tqdm=None):
     while len(boards) < boards.size:
         game = chess.pgn.read_game(pgn)
-        board = game.board()
-        for move in game.mainline_moves():
-            board.push(move)
-            if boards[board] is not None: continue
+        if game is None: continue
 
-            _, score = insert_evaluation(board, engine, conn)
-            boards[board] = board
-
-            print(f"finished board {len(boards)}/{boards.size} with score {score}.")
+        fn = lambda x,y: pbar.update()
+        insert_evaluations(game, boards, engine, conn, None if pbar is None else fn)
 
 def main():
-    pgn = open(os.getenv("GAMES_PATH"))
-    boards = hashtable.HashTable(int(os.getenv("BOARDS_SIZE")))
-    engine = chess.engine.SimpleEngine.popen_uci(os.getenv("STOCKFISH_PATH"))
-    conn = sqlite3.connect("evaluations.db")
+    games_path, stockfish_path = os.getenv("GAMES_PATH"), os.getenv("STOCKFISH_PATH")
+    if games_path is None or stockfish_path is None:
+        raise KeyError("could not find environment variables")
 
-    conn.execute('''
+    pgn = open(games_path)
+    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+    conn = sqlite3.connect("evaluations.db")
+    cur = conn.cursor()
+    boards = hashtable.EvaluationsHashTable(int(os.getenv("BOARDS_SIZE", 10007)), cur)
+    pbar = tqdm(total=boards.size-len(boards), desc="Loading games")
+
+    conn.execute("""
         CREATE TABLE IF NOT EXISTS evaluations (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            zobrist BLOB PRIMARY KEY,
             binary BLOB NOT NULL,
             score INT NOT NULL
         )
-    ''')
+    """)
 
     conn.commit()
 
-    insert_pgn(pgn, boards, engine, conn)
+    insert_pgn(pgn, boards, engine, conn, pbar)
     conn.close()
     engine.quit()
 
